b2fb172e5be4c4106a8cee1a5e00b5340ed5589f
Mahmoud Elkomy <82784354+mahmoudelkomy996@users.noreply.github.com>
2023-11-27 08:59:39 +0200

    home

---
+++ js/custom.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+  // favicon based on color scheme
+  function setFavicon() {
+    const darkFavicon = $("#browser-dark-theme-favicon");
+    const lightFavicon = $("#browser-light-theme-favicon");
+    if (darkFavicon.length > 0 && lightFavicon.length > 0) {
+      const isDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      if (isDarkMode) {
+        lightFavicon.removeAttr("href");
+        // lightFavicon.attr("disabled", "disabled");
+      } else {
+        darkFavicon.removeAttr("href");
+        // darkFavicon.attr("disabled", "disabled");
+      }
+    }
+  }
+  setFavicon();
+  // listen for changes in the color scheme and update the favicon
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      setFavicon();
+    });
+
+  $(".navbar-toggler").on("click", function () {
+    // Toggle the "show" class on the navbar-nav element
+    $(".navbar-nav").toggleClass("show");
+  });
+
+  // Add a click event listener to the document
+  $(document).on("click", function (event) {
+    // Check if the clicked element is not part of the navbar
+    if (!$(event.target).closest(".navbar").length) {
+      // Remove the "show" class from the navbar-nav element
+      $(".navbar-nav").removeClass("show");
+    }
+  });
+
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+});
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+$(document).ready(function () {
+  //spinner
+  $(".preloader").delay(1000).fadeOut(300);
+  //aos Delay
+  $("section").each(function () {
+    const sectionDivs = $(this).find("[data-aos]");
+    sectionDivs.each(function (index) {
+      $(this).attr("data-aos-delay", (index + 1) * 100);
+    });
+  });
+  // aos
+  AOS.init({
+    offset: 20,
+    delay: 50,
+    duration: 750,
+    once: true,
+  });
+  // lozad
+  const observer = lozad(".lazy", {
+    loaded: function (el) {
+      el.parentNode.classList.add("loaded");
+    },
+  });
+  observer.observe();
+  // parallax
+  var parallaxImage = document.getElementsByClassName("parallax");
+  new simpleParallax(parallaxImage, {
+    delay: 1,
+    transition: "cubic-bezier(0,0,0,1)",
+  });
+  // tooltip
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+  // counter up
+  const counterUp = window.counterUp.default;
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      const el = entry.target;
+      if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+        counterUp(el, {
+          duration: 3000,
+          delay: 16,
+        });
+        el.classList.add("is-visible");
+      }
+    });
+  };
+  const IO = new IntersectionObserver(callback, { threshold: 1 });
+  const elements = document.querySelectorAll(".counterUp");
+  elements.forEach((el) => IO.observe(el));
+});
